d2515b7c413561b039c178be30045bcbc5a0bdf3
hillaryhosea <107868525+hillaryhosea@users.noreply.github.com>
2023-09-28 23:53:01 +0300

    new changes

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 
+var randomnumber1  = Math.floor(Math.random() *6 ) + 1;
+
+var randomDiceImage = "dice" + randomnumber1 + ".png";
+
+var randomImage = "images/" + randomDiceImage ;
+
+var image1 = document.querySelectorAll ;
+
+
