d5b18138c449dbab623c4ee90d7d9923c29d5edf
hillaryhosea <107868525+hillaryhosea@users.noreply.github.com>
2023-09-29 13:25:58 +0300

    additional adjustment

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 
-var randomnumber1  = Math.floor(Math.random() *6 ) + 1;
+var randomNumber1  = Math.floor(Math.random() *6 ) + 1;
 
-var randomDiceImage = "dice" + randomnumber1 + ".png";
+var randomDiceImage = "dice" + randomNumber1 + ".png";
 
 var randomImage = "images/" + randomDiceImage ;
 
-var image1 = document.querySelectorAll ;
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImage);
+
+
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+
+var randomImage2 = "images/" + randomDiceImage2;
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomImage2);
+
+if (randomNumber1 > randomNumber2) {
+
+    document.querySelector("h1").innerHTML = "Player 1 won !!";
+
+} else if (randomNumber2 > randomNumber1) {
+
+    document.querySelector("h1").innerHTML = "player 2 won !!";
+
+} else {
+    document.querySelector ("h1").innerHTML = "Draw !!";
+}
+
+const refresh = document.getElementById("btn");
+
+refresh.addEventListener("click", function () {
+
+    location.reload();
+});
 
 
